5c95273164eb984e31af2b14da17497553c31f97
San Kim <waterfog9580@gmail.com>
2023-12-01 00:19:02 +0900

    ✅ 동시성 테스트 작성

--- api/src/test/kotlin/com/example/api/service/CouponServiceIntegrationTest.kt
+++ api/src/test/kotlin/com/example/api/service/CouponServiceIntegrationTest.kt
@@ -4,6 +4,9 @@ import com.example.api.repository.CouponRepository
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class CouponServiceIntegrationTest(
@@ -12,7 +15,7 @@ class CouponServiceIntegrationTest(
 ) : DescribeSpec({
 
 
-    describe("쿠폰 응모 요청시") {
+    describe("쿠폰 응모 요청") {
 
         it("새로운 쿠폰을 생성한다") {
             couponService.apply(1L)
@@ -20,6 +23,27 @@ class CouponServiceIntegrationTest(
             val count: Long = couponRepository.count()
 
             count shouldBe 1
+        }
+
+        context("여러명이 동시에 요청한다") {
+            val threadCount = 1000;
+            val executorService: ExecutorService = Executors.newFixedThreadPool(threadCount)
+            val latch = CountDownLatch(threadCount)
+
+            repeat(threadCount) {
+                executorService.submit {
+                    try {
+                        couponService.apply(1L)
+                    } finally {
+                        latch.countDown()
+                    }
+                }
+            }
+
+            latch.await()
+
+            val count = couponRepository.count()
+            count shouldBe 100
         }
     }
 })
