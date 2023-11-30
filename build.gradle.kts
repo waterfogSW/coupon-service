7b837bfd87bc245ab968765f3f60ccf58ce06e2a
San Kim <waterfog9580@gmail.com>
2023-11-30 16:56:53 +0900

    🎉 프로젝트 생성

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "org.waterfogsw"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
