fc83dfbf54d88f64022ffd80a484511ab7f86edf
San Kim <waterfog9580@gmail.com>
2023-12-01 01:32:20 +0900

    🔧 Redis 설정

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,10 +7,14 @@ jar.enabled = true
 bootJar.enabled = false
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Version.SPRING_BOOT}")
     implementation("org.springframework.boot:spring-boot-starter-web:${Version.SPRING_BOOT}")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Version.SPRING_BOOT}")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:${Version.SPRING_BOOT}")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools:${Version.SPRING_BOOT}")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose:${Version.SPRING_BOOT}")
+
     runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
 
     testImplementation("com.h2database:h2:2.1.214")
