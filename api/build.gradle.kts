9313ba4d2879b66a02a4c8a780f5bd48867a7dd1
San Kim <waterfog9580@gmail.com>
2023-12-07 16:56:30 +0900

    ➕ Kafka 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,13 +9,23 @@ bootJar.enabled = false
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:${Version.SPRING_BOOT}")
 
+    // jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Version.SPRING_BOOT}")
+
+    // redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis:${Version.SPRING_BOOT}")
 
+    // kafka
+    implementation("org.springframework.kafka:spring-kafka:3.1.0")
+
+    // devtools
     developmentOnly("org.springframework.boot:spring-boot-devtools:${Version.SPRING_BOOT}")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose:${Version.SPRING_BOOT}")
 
+    // mysql
     runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
 
+    // test database
     testImplementation("com.h2database:h2:2.1.214")
+
 }
