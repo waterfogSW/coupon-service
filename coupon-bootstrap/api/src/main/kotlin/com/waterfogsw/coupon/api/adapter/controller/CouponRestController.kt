b05c26f8dad7f9a29d94342f10dd04d30ba3722f
San Kim <waterfog9580@gmail.com>
2023-12-21 18:40:59 +0900

    🩹 중복 발급시 BadRequest 응답으로 수정

--- coupon-bootstrap/api/src/main/kotlin/com/waterfogsw/coupon/api/adapter/controller/CouponRestController.kt
+++ coupon-bootstrap/api/src/main/kotlin/com/waterfogsw/coupon/api/adapter/controller/CouponRestController.kt
@@ -2,6 +2,7 @@ package com.waterfogsw.coupon.api.adapter.controller
 
 import com.waterfogsw.coupon.api.adapter.dto.IssueCouponRequest
 import com.waterfogsw.coupon.usecase.usecase.IssueCouponUseCase
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,12 +18,16 @@ class CouponRestController(
     fun issueCoupon(
         @RequestBody
         request: IssueCouponRequest
-    ) {
+    ): ResponseEntity<Unit> {
         val command: IssueCouponUseCase.Command =
             IssueCouponUseCase.Command(
                 userId = request.userId
             )
 
-        issueCouponUseCase.invoke(command)
+        val result = issueCouponUseCase.invoke(command)
+        return when (result) {
+            is IssueCouponUseCase.Result.Success -> ResponseEntity.ok().build()
+            is IssueCouponUseCase.Result.Failure -> ResponseEntity.badRequest().build()
+        }
     }
 }
