824cdb579d004e47d601ffbe4939cd6961f3870b
San Kim <waterfog9580@gmail.com>
2023-12-21 20:46:07 +0900

    🔥 transactional 제거

--- coupon-infrastructure/persistence/src/main/kotlin/com/waterfogsw/coupon/persistence/adapter/jpa/CouponJpaAdapter.kt
+++ coupon-infrastructure/persistence/src/main/kotlin/com/waterfogsw/coupon/persistence/adapter/jpa/CouponJpaAdapter.kt
@@ -5,14 +5,12 @@ import com.waterfogsw.coupon.persistence.adapter.jpa.entity.CouponJpaEntity
 import com.waterfogsw.coupon.persistence.adapter.jpa.repository.CouponJpaRepository
 import com.waterfogsw.coupon.usecase.port.CouponRepository
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 @Component
 class CouponJpaAdapter(
     private val couponJpaRepository: CouponJpaRepository
 ) : CouponRepository {
 
-    @Transactional
     override fun save(coupon: Coupon) {
         couponJpaRepository.save(CouponJpaEntity.from(coupon))
     }
